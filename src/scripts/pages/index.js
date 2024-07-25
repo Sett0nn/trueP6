9d28ea094a205c355790c092502a855b01d24348
Sett0nn <pooktommy@hotmail.com>
2024-07-25 09:48:16 +0200

    photographer.js maj

--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -2,6 +2,30 @@ async function getPhotographers() {
     // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
     // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
 
+    fetch('scripts/utils/photographer.json')
+        .then(response => response.json())
+        .then(data => {
+            const params = new URLSearchParams(window.location.search);
+            const photographId = parseInt(params.get('photograph_id'));
+            let photographInfo = data.find(element => element.id === photographId);
+
+
+            let nameElement = document.getElementById("fullname")
+            nameElement.textContent = photographInfo.fullname;
+
+            let townElement = document.getElementById("town")
+            townElement.textContent = photographInfo.localisation;
+
+            let profilElement = document.getElementById("profil")
+            profilElement.src = photographInfo.url
+
+            if    (!response.ok){
+                throw new Error('Network response was not ok ' + response.statuText);
+            }
+        });
+
+
+
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
--- src/scripts/pages/photographer.js
+++ src/scripts/pages/photographer.js
@@ -5,5 +5,75 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("src/utils/photographer.json")
         .then(response => response.json())
         .then(data => {
-        })
+            const params = new URLSearchParams(window.location.search);
+            const photographId = parseInt(params.get('photograph_id'));
+           let photographInfo = data.find(element => element.id === photographId);
+
+            let nameElement = document.getElementById("fullname")
+            nameElement.textContent = photographInfo.fullname;
+
+            let townElement = document.getElementById("town")
+            townElement.textContent = photographInfo.localisation;
+
+            let profilElement = document.getElementById("profil")
+            profilElement.src = photographInfo.url;
+
+            let descriptionElement = document.getElementById("description")
+            descriptionElement.textContent = photographInfo.description;
+
+       })
 })
+
+async function getPhotographerData() {
+    try{
+        const response = await fetch("src/utils/photographer.json")
+        .then(response => response.json())
+    } catch (error) {
+        console.error(error)
+     }
+    }
+
+    let PhotographerData = {}
+
+ getPhotographerData().then(data => PhotographerData = data)
+
+const photographerEvent = document.querySelectorAll("a").forEach(a=>{
+    a.addEventListener('click', photographerInfo)
+})
+
+function photographerInfo(event) {
+    let photographerChoice = PhotographData.photographers.find(photographer => {
+        return event.target.id === photographer.name.toLowerCase()
+        return event.target.id === photographer.city.toLowerCase()
+        return event.target.id === photographer.country.toLowerCase()
+        return event.target.id === photographer.tagline.toLowerCase()
+        return event.target.id === photographer.price.toLowerCase()
+        return event.target.id === photographer.picture.toLowerCase()
+    })
+
+    console.log(photographerChoice)
+
+    document.querySelector("#photographerName").textContent = `${photographerChoice.name} photographer`
+    document.querySelector("#city").textContent = `${photographerChoice.city}`
+    document.querySelector("#country").textContent = `${photographerChoice.country}`
+    document.querySelector("#tagline").textContent = `${photographerChoice.tagline}`
+    document.querySelector("#price").textContent = `${photographerChoice.price}`
+    document.querySelector("#picture").innerHTML = `${photographerChoice.picture}`
+
+}
+
+
+
+
+
+
+
+
+//const photographId = document.getElementById("photograph_id");
+//const imagesIndexes = [1,2,3,4,5,6];
+//const selectIndex = null;
+
+//imagesIndexes.forEach(i =>{
+    //const image = document.createElement("img");
+    //image.src = `/`
+//})
